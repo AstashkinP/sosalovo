123f3e9a7bf3b14abce86f914595ef7e9040b010
student4 <stud4@STUDENT4>
2025-11-03 17:57:17 +0700

    доедалать кубы/сделать кубам коллизию или сделать ракетку

--- sosalovo/sosalovo.cpp
+++ sosalovo/sosalovo.cpp
@@ -1,14 +1,16 @@
 ﻿//kasjbdnksdjasdwasdwa﻿
 #include "framework.h"
-#include "Abracadabra.h"
+#include "sosalovo.h"
 #include <iostream>
 #pragma comment (lib, "Msimg32.lib")
 #pragma comment (lib, "winmm.lib")
 #include <cmath>
 
-#ifndef UNICODE
+//#ifndef UNICODE
 #define UNICODE
-#endif 
+#define COLUMN 5
+#define ROWS 4
+//#endif 
 
 typedef struct {
     float x, y, width, height, dx, dy, speed;
@@ -26,11 +28,12 @@ struct {
     HBITMAP hBack;
 } window;
 
+
 bool gameAction = false;
 
 //HBITMAP hBack;// хэндл для фонового изображения
 
-
+sprite blocks[ROWS][COLUMN];
 
 //#define MAX_LOADSTRING 100
 
@@ -59,6 +62,20 @@ void InitGame()
     ball.y = racket.y - ball.height;
     ball.dy = (35 + rand() % 65) / 100.;
     ball.dx = ball.dy - 1;
+
+    for (int i = 0; i < ROWS; i++) {
+
+        for (int j = 0; j < COLUMN; j++) {
+
+            blocks[i][j].width = 40;
+            blocks[i][j].height = 40;
+            blocks[i][j].hBitmap = (HBITMAP)LoadImageA(NULL, "cube.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+            blocks[i][j].x = blocks[i][j].width * i;
+            blocks[i][j].y = blocks[i][j].height * j;
+
+        }
+
+    }
 }
 
 void InitWindow() { // инициализация структуры window
@@ -118,6 +135,8 @@ void Collusion()
     if (ball.y > window.height) {
         ball.x = racket.x + racket.width / 2;
         ball.y = racket.y - ball.height;
+        ball.dy = (35 + rand() % 65) / 100.;
+        ball.dx = ball.dy - 1;
         gameAction = false;
     }
 }
@@ -136,6 +155,18 @@ void ProcessGame()
         Obey();
         Collusion();
         ProcessBall();
+    }
+}
+
+void Show()
+{
+    ShowBitMap(window.device_context, 0, 0, window.width, window.height, window.hBack);
+    ShowBitMap(window.device_context, ball.x, ball.y, ball.width, ball.height, ball.hBitmap);
+    ShowBitMap(window.device_context, racket.x, racket.y, racket.width, racket.height, racket.hBitmap);
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMN; j++) {
+            ShowBitMap(window.device_context, blocks[i][j].x, blocks[i][j].y, blocks[i][j].width, blocks[i][j].height, blocks[i][j].hBitmap);
+        }
     }
 }
 
@@ -217,8 +248,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_DESTROY:
-        PostQuitMessage(0);
+            PostQuitMessage(0);
+        if (GetAsyncKeyState(VK_ESCAPE)) {
 
+        }
         return 0;
 
     case WM_SIZE:
@@ -237,9 +270,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         window.device_context = BeginPaint(hwnd, &ps);
 
         //FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        ShowBitMap(window.device_context, 0, 0, window.width, window.height, window.hBack);
-        ShowBitMap(window.device_context, ball.x, ball.y, ball.width, ball.height, ball.hBitmap);
-        ShowBitMap(window.device_context, racket.x, racket.y, racket.width, racket.height, racket.hBitmap);
+        Show();
         EndPaint(hwnd, &ps);
     }
     return 0;
@@ -247,146 +278,3 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
-
-//int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-//                     _In_opt_ HINSTANCE hPrevInstance,
-//                     _In_ PWSTR    lpCmdLine,
-//                     _In_ int       nCmdShow)
-//{
-//    UNREFERENCED_PARAMETER(hPrevInstance);
-//    UNREFERENCED_PARAMETER(lpCmdLine);
-//
-//    // TODO: Разместите код здесь.
-//
-//    // Инициализация глобальных строк
-//    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-//    LoadStringW(hInstance, IDC_ABRACADABRA, szWindowClass, MAX_LOADSTRING);
-//    InitWindow();
-//    InitGame();
-//
-//    // Выполнить инициализацию приложения:
-//    if (!InitInstance (hInstance, nCmdShow))
-//    {
-//        return FALSE;
-//    }
-//
-//    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ABRACADABRA));
-//
-//    MSG msg;
-//
-//    // Цикл основного сообщения:
-//    while (GetMessage(&msg, nullptr, 0, 0))
-//    {
-//        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-//        {
-//            TranslateMessage(&msg);
-//            DispatchMessage(&msg);
-//        }
-//    }
-//
-//    return (int) msg.wParam;
-//}
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  ЦЕЛЬ: Регистрирует класс окна.
-//
-
-
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
-//
-//   КОММЕНТАРИИ:
-//
-//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится главное окно программы.
-//
-//BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-//{
-//   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
-//
-//   window.hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-//      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-//
-//   if (!window.hWnd)
-//   {
-//      return FALSE;
-//   }
-//
-//   ShowWindow(window.hWnd, nCmdShow);
-//   UpdateWindow(window.hWnd);
-//   
-//   return TRUE;
-//}
-
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND  - обработать меню приложения
-//  WM_PAINT    - Отрисовка главного окна
-//  WM_DESTROY  - отправить сообщение о выходе и вернуться
-
-//LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-//{
-//    switch (message)
-//    {
-//    //case WM_COMMAND:
-//    //    {
-//    //        int wmId = LOWORD(wParam);
-//    //        // Разобрать выбор в меню:
-//    //        switch (wmId)
-//    //        {
-//    //        case IDM_ABOUT:
-//    //            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-//    //            break;
-//    //        case IDM_EXIT:
-//    //            DestroyWindow(hWnd);
-//    //            break;
-//    //        default:
-//    //            return DefWindowProc(hWnd, message, wParam, lParam);
-//    //        }
-//    //    }
-//    //    break;
-//    case WM_PAINT:
-//        {
-//            PAINTSTRUCT ps;
-//            HDC hdc = BeginPaint(hWnd, &ps);
-//            ShowBitmap(hdc, ball.x, ball.y, ball.width, ball.height, ball.hBitmap, false);
-//            ShowBitmap(hdc, 0, 0, window.width, window.height, hBack);
-//            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-//            EndPaint(hWnd, &ps);
-//        }
-//        break;
-//    case WM_DESTROY:
-//        PostQuitMessage(0);
-//        break;
-//    default:
-//        return DefWindowProc(hWnd, message, wParam, lParam);
-//    }
-//    return 0;
-//}
-
-// Обработчик сообщений для окна "О программе".
-//INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-//{
-//    UNREFERENCED_PARAMETER(lParam);
-//    switch (message)
-//    {
-//    case WM_INITDIALOG:
-//        return (INT_PTR)TRUE;
-//
-//    case WM_COMMAND:
-//        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-//        {
-//            EndDialog(hDlg, LOWORD(wParam));
-//            return (INT_PTR)TRUE;
-//        }
-//        break;
-//    }
-//    return (INT_PTR)FALSE;
-//}
